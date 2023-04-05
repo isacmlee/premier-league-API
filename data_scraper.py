cf03c2cf905a06ad588f6f84664734afd2e6b21e
Isac Lee <isacmlee@gmail.com>
2023-04-04 22:20:27 -0700

    exploring elements

--- data_scraper.py
+++ data_scraper.py
@@ -5,3 +5,16 @@ URL = "https://www.premierleague.com/stats"
 page = requests.get(URL)
 
 soup = BeautifulSoup(page.content, "html.parser")
+
+results = soup.find(id="mainContent")
+results = results.find("div",class_="hasSideNav")
+results = results.find("div",class_="sidebarPush")
+results = results.find_all("div",class_="col-12")
+stats = results[1].find_all("section",class_="mainWidget statsRow DashboardPlayerStats")
+stats = stats[0].find("ul",class_="block-list-4 mobileScrollList")
+children = stats.findChildren("li")
+print(children)
+
+
+# results = soup.find_all("div", class_="")
+print(stats)
