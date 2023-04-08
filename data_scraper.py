4253179d28f216be9eb039520c7a3e2d40f42f5a
Isac Lee <isacmlee@gmail.com>
2023-04-07 21:25:22 -0700

    I'm so confused

--- data_scraper.py
+++ data_scraper.py
@@ -1,20 +1,24 @@
 import requests
+from selenium import webdriver 
 from bs4 import BeautifulSoup
 
-URL = "https://www.premierleague.com/stats"
-page = requests.get(URL)
+# using selenium to grab JS rendered data
+browser = webdriver.Chrome()
+browser.get("https://www.premierleague.com/stats")
+html_source = browser.page_source
+browser.quit()
 
-soup = BeautifulSoup(page.content, "html.parser")
+# BeautifulSoup parses html_source
+soup = BeautifulSoup(html_source, "html.parser")
 
 results = soup.find(id="mainContent")
 results = results.find("div",class_="hasSideNav")
 results = results.find("div",class_="sidebarPush")
 results = results.find_all("div",class_="col-12")
-stats = results[1].find_all("section",class_="mainWidget statsRow DashboardPlayerStats")
+stats = results[1].find("section",class_="mainWidget statsRow DashboardPlayerStats")
 stats = stats[0].find("ul",class_="block-list-4 mobileScrollList")
 children = stats.findChildren("li")
 print(children)
 
 
 # results = soup.find_all("div", class_="")
-print(stats)
